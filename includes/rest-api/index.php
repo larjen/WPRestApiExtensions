d5df085f97d7509f883d4569b8b68a1c10ff746d
Lars Jensen <lars.jensen@exenova.dk>
2015-11-08 20:32:47 +0100

    Adding functionality to wipe cache.

--- includes/rest-api/index.php
+++ includes/rest-api/index.php
@@ -5,7 +5,7 @@
  */
 
 define("CACHE_DIRECTORY", __DIR__ . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR);
-define("PATH_TO_WP","");
+define("PATH_TO_WP", "");
 
 /*
  * Add headers
@@ -13,6 +13,7 @@ define("PATH_TO_WP","");
 
 function add_headers($status_code) {
     header($_SERVER["SERVER_PROTOCOL"] . " " . $status_code);
+    header("Content-Type: application/json; charset=UTF-8");
 }
 
 /*
@@ -32,19 +33,19 @@ function wipe_cache() {
  */
 
 function get_fresh($cache_key) {
-    
+
     // construct a new request but strip the _jsonp from the request
     $request_parameters = "?";
     foreach ($_GET as $key => $value) {
-        if ($key != "_jsonp"){
-            $request_parameters = $request_parameters."&".$key."=".$value;
+        if ($key != "_jsonp") {
+            $request_parameters = $request_parameters . "&" . $key . "=" . $value;
         }
     }
-    
-    $URI = "http://" . $_SERVER["HTTP_HOST"] . PATH_TO_WP . "/wp-json/wprestapiextensions/v1/".ENDPOINT."/". $request_parameters;
-    
+
+    $URI = "http://" . $_SERVER["HTTP_HOST"] . PATH_TO_WP . "/wp-json/wprestapiextensions/v1/" . ENDPOINT . "/" . $request_parameters;
+
     error_log($URI);
-    
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $URI);
     curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -57,8 +58,8 @@ function get_fresh($cache_key) {
     curl_close($ch);
 
     // print the result
-    print_result($output,$status_code);
-    
+    print_result($output, $status_code);
+
     // now add file to cache
     $fh = fopen(CACHE_DIRECTORY . $cache_key, 'w') or die();
     fwrite($fh, $status_code . ":");
@@ -69,34 +70,36 @@ function get_fresh($cache_key) {
 /*
  * Gets the cachekey for the request
  */
-function get_cache_key(){
+
+function get_cache_key() {
     $cache_key = "";
     foreach ($_GET as $key => $value) {
-        if ($key != "_jsonp"){
+        if ($key != "_jsonp") {
             $cache_key = $cache_key . $key . $value;
         }
     }
-    return ENDPOINT.$cache_key;
+    return ENDPOINT . $cache_key;
 }
 
 /*
  * Prints the result, and decide if it should be wrapped in
  * a jsonp callback
  */
-function print_result($output, $status_code){
-    
+
+function print_result($output, $status_code) {
+
     // add headers
     add_headers($status_code);
-    
+
     // now print it
     if (isset($_GET["_jsonp"])) {
-        
+
         // wrap in jsonp format
-        print("/**/".$_GET["_jsonp"]."(");
-        print($output);    
+        print("/**/" . $_GET["_jsonp"] . "(");
+        print($output);
         print(")");
-    }else{
-        print($output);    
+    } else {
+        print($output);
     }
 }
 
@@ -113,20 +116,20 @@ function get_cached($cache_key) {
     }
 
     $output = file_get_contents(CACHE_DIRECTORY . $cache_key);
-    $output_array = explode(":", $output, 2); 
-    
+    $output_array = explode(":", $output, 2);
+
     // print the result
-    print_result($output_array[1],$output_array[0]);
+    print_result($output_array[1], $output_array[0]);
 }
 
 // if purge set - wipe cache
-if (isset($_GET["PURGE_CACHE"]) && !defined('ENDPOINT') ) {
+if (isset($_GET["PURGE_CACHE"]) && !defined('ENDPOINT')) {
     wipe_cache();
 } else {
     // check if response is in cache
-    if (defined('ENDPOINT')){
+    if (defined('ENDPOINT')) {
         $cache_key = get_cache_key();
-        
+
         if (get_cached($cache_key) === false) {
             get_fresh($cache_key);
         }
